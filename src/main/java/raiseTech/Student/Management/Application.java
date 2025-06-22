4b549893cd2014a735f77a8a1833848d9c17f2c8
sen <shiro.ma273rssnrchi63@gmail.com>
2025-06-22 12:31:06 +0900

    初回コミット

---
+++ src/main/java/raiseTech/Student/Management/Application.java
@@ -0,0 +1,20 @@
+package raiseTech.Student.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+@GetMapping("/Hello")
+	public  String Hello(){
+		return  "Hello,World! Today is WednesDay.";
+}
+
+}
