5966d1476ca4d88b7ab9bdd3bbafce97895011c0
sen <shiro.ma273rssnrchi63@gmail.com>
2025-06-28 09:34:04 +0900

    PostMappingにãを用いてãMapに情報を追加する処理を追加しました

--- src/main/java/raiseTech/Student/Management/Application.java
+++ src/main/java/raiseTech/Student/Management/Application.java
@@ -1,13 +1,14 @@
 package raiseTech.Student.Management;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,42 +18,44 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-@GetMapping("/Hello")
-	public  String Hello(){
-		return  "Hello,World! Today is WednesDay.";
-}
 
-private String name ="Yamada Hanako";
-	private  String age = "30";
+	private String name = "Yamada Hanako";
+	private String age = "30";
 
 	@GetMapping("/StudentInfo")
-	public String getStidentInfo(){
-		return name+" "+ age +"歳";
+	public String getStidentInfo() {
+		return name + " " + age + "歳";
 	}
+
 	@PostMapping("/StudentInfo")
-	public  void  setName(String name,String age){
+	public void setName(String name, String age) {
 		this.name = name;
 		this.age = age;
 	}
 
 
-	private Map<String, String> studentdate =new HashMap<>();
+	private Map<String, String> studentData = new HashMap<>();
 
 	{
-		studentdate.put("hanako","25");
-		studentdate.put("taro","30");
+		studentData.put("hanako", "25");
+		studentData.put("taro", "30");
 	}
 
 
 	@GetMapping("/studentInfo")
-	public String getstudentInfo(){
-		String result = studentdate.entrySet().stream()
-				.map(entry ->entry.getKey()+"は"+entry.getValue() + "歳です。")
+	public String getstudentInfo() {
+		String result = studentData.entrySet().stream()
+				.map(entry -> entry.getKey() + "は" + entry.getValue() + "歳です。")
 				.collect(Collectors.joining("\n"));
 
 		return result;
 	}
 
+	@PostMapping("/studentInfo")
+public String addStudent(@RequestBody Student request){
+		studentData.put(request.getStudentName(),request.getStudentAge());
+		return "名前："+request.getStudentName()+"年齢："+request.getStudentAge()+"を追加しました。";
+	}
 
 
 }
---
+++ src/main/java/raiseTech/Student/Management/Student.java
@@ -0,0 +1,23 @@
+package raiseTech.Student.Management;
+
+public class Student {
+
+  public String getStudentName() {
+    return studentName;
+  }
+
+  public void setStudentName(String studentName) {
+    this.studentName = studentName;
+  }
+
+  public String getStudentAge() {
+    return studentAge;
+  }
+
+
+  private  String studentName;
+  private  String studentAge;
+
+
+
+}
